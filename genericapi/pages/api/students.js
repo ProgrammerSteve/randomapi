593afb89c351ded03a51bd382df48507a57e5236
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-06-03 22:19:44 -0500

    adding cors to serverless function

--- genericapi/pages/api/students.js
+++ genericapi/pages/api/students.js
@@ -1,3 +1,4 @@
+
 const students= 
     {students:
         [
@@ -285,6 +286,49 @@ const students=
 
 
 
-export default function handler(req, res) {
+
+
+
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
+  
+//   const handler = (req, res) => {
+//     const d = new Date()
+//     res.end(d.toString())
+//   }
+
+
+
+ const handler=(req, res)=> {
     res.status(200).json(students)
   }
+
+
+
+  
+ export default allowCors(handler);
+
+
+
+
+
+
+
+
+
+
+
