6ac210f68687c2f75f435480d683bfd35e95e961
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-09-07 20:39:19 -0500

    fixed the link to auth0 routes

--- genericapi/pages/index.js
+++ genericapi/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
 import styles from '../styles/Home.module.css'
 import { useState, useEffect } from 'react';
 import { useUser } from '@auth0/nextjs-auth0';
@@ -18,8 +19,16 @@ export default function Home() {
       <div className='my-5 py-2'>
         <div className='mb-2'><p className='text-xl'>Auth0 Sign In/ Logout</p></div>
         <div className='flex gap-3'>
-          <div><a href="/api/auth/login" className='w-24 h-12 px-2 py-1 bg-white text-black border-2 border-black rounded-lg' >Log In</a></div>
-          <div><a href="/api/auth/logout" className='w-24 h-12 px-2 py-1 bg-black text-gray-300 border-2 border-gray-300 rounded-lg' >Log Out</a></div>
+          <div>
+            <Link href={"/api/auth/login"}>
+            <a className='cursor-pointer w-24 h-12 px-2 py-1 bg-white text-black border-2 border-black rounded-lg' >Log In</a>
+            </Link>
+            </div>
+          <div>
+            <Link href={"/api/auth/logout"}>
+            <a className='cursor-pointer w-24 h-12 px-2 py-1 bg-black text-gray-300 border-2 border-gray-300 rounded-lg' >Log Out</a>
+            </Link>
+          </div>
         </div>
       </div>
       {
--- genericapi/pages/signin.js
+++ genericapi/pages/signin.js
@@ -15,24 +15,7 @@ export default function Signin() {
 
   return (
     <div>
-      <div className='my-5 py-2'>
-        <div className='mb-2'><p className='text-xl'>Auth0 Sign In/ Logout</p></div>
-        <div className='flex gap-3'>
-          <div><a href="/api/auth/login" className='w-24 h-12 px-2 py-1 bg-white text-black border-2 border-black rounded-lg' >Log In</a></div>
-          <div><a href="/api/auth/logout" className='w-24 h-12 px-2 py-1 bg-black text-gray-300 border-2 border-gray-300 rounded-lg' >Log Out</a></div>
-        </div>
-      </div>
-      {
-        isLoading &&
-        <div>Loading ...</div>
-      }
-      {
-      user  && 
-        <div>
-          <h2>Name: {user.name}</h2>
-          <p>Email: {user.email}</p>
-        </div>
-      }
+
     </div>
   )
 }
